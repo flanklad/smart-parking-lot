0faa7d61bcd0a1891500e6d8e598f0e00f8c8e4c
flanklad <sidak450@gmail.com>
2026-08-11 16:09:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from view import ParkingView
 from custom_errors.exceptions import SlotNotAvailableError, VehicleNotFoundError, InvalidVehicleTypeError
 
 logging.basicConfig(
-      force=True,
       level=logging.DEBUG,
       format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
       datefmt="%d-%b-%y %H:%M:%S",
